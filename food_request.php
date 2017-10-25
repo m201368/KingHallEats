ae7f40fe515b8336dd16e3956dec6c18dcbec1b9
bbirney <ben.birney@gmail.com>
2017-10-24 21:30:51 -0400

    Uploaded food_request.php
    Generates form for user (with stand-in food items that will be updated for later checkpoints). Appends to "Logs/requests.php" each food request in the format: "username; food item; additional comment; time of request; status (incomplete/complete);". I figured Logs could be used for other logged info too (like usernames/passwords).

--- food_request.php
+++ food_request.php
@@ -1,1 +1,44 @@
+<!Doctype HTML>
+<html>
 
+<head>
+  <style type="text/css">
+    table, td, th {
+      border: 1px solid black;
+    }
+  </style>
+  <meta charset="UTF-8">
+  <meta name="description" content="Food Request for KingHallEats">
+  <meta name="keywords" content="King, Hall, Eats, Food, Request">
+  <meta name="author" content="Ben Birney">
+  <title>Food Request</title>
+  <script type="text/javascript" src="pscripts.js"></script>
+</head>
+
+<body bgcolor=#aaaaa>
+  <?php
+  if (isset($_POST['sFood'])) {
+    $file = fopen("Logs/requests.txt", 'a') or die("can't open file");
+    $order = "username; ".serialize($_POST['sFood'])."; ".serialize($_POST['com'])."; ".serialize(time())."; incomplete;\n";
+    fwrite($file, $order);
+    fclose($file);
+    print "<b>Thanks for submitting a request!</b>";
+  } else { ?>
+    <form method='Post' action='?'>
+      <select id="cat" onchange="specificRequest()">
+        <option value=" ">Food Category</option>
+        <option value="meal">Meal</option>
+        <option value="dairy">Dairy</option>
+        <option value="cereal">Cereal</option>
+        <option value="produce">Produce</option>
+        <option value="snax">Snacks</option>
+      </select>
+      <br>
+      <div id="specificFood"></div>
+      <input type="text" name="com" placeholder="Comments">
+      <input type="Submit" value="Submit">
+    </form>
+  <?php } ?>
+</body>
+
+</html>
