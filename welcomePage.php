39d0bbeba9a8358285346b31e760abf161443786
sasbarkley <sasbarkley@gmail.com>
2017-10-25 20:44:42 -0400

    added bootstrap directory and welcomePage

---
+++ welcomePage.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>KHE: Home</title>
+
+    <!-- Bootstrap -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <style type="text/css">
+    body{
+      margin-left: 10%;
+      margin-right: 10%;
+      margin-top: 10%;
+      margin-bottom: 10%;
+      padding: 10px 10px 10px 10px;
+      font-family: 'Josefin Sans', sans-serif;
+      text-align: center;
+    }
+    </style>
+</head>
+<body>
+  <h1>Welcome to King Hall Eats!</h1>
+  <h4>Based on the widely used "UberEats", King Hall Eats aims to serve the United States Naval Academy Brigade of Midshipmen through door-to-door food delivery within Bancroft Hall. This service is provided by mids for mids!</h4>
+</body>
+</html>
