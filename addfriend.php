d595d5db6ceb4c13c936b16cd615692b7816e3ce
m201368 <33030039+m201368@users.noreply.github.com>
2017-11-06 10:38:54 -0500

    changed to work with session variable

--- addfriend.php
+++ addfriend.php
@@ -1,4 +1,13 @@
 <?php
+  if(!isset($_SESSION['user'])) {
+    session_start();
+    ?><script type="text/javascript">
+      document.location = "welcomePage.php";
+    </script><?php
+  }
+?>
+
+<?php
   //returns array of all friends of person with inputed name from friends file
   function showyourfriends($name){
     $array;
@@ -22,9 +31,9 @@
   else{
     //add name, friend to friends.txt in each new line
     $file = fopen("Logs/friends.txt",'a');
-    $data = $_POST['name'].", ".$_POST['friend']."\n";
+    $data = $_SESSION['user'].", ".$_POST['friend']."\n";
     fwrite($file,$data);
-    if($_POST['user']!=""){echo"Thanks for adding a friend!";}
+    if($_SESSION['user']!=""){echo"Thanks for adding a friend!";}
   }
   // used the lines to make sure functions were working properly
   // $answer=showyourfriends($_POST['name']);
@@ -50,7 +59,7 @@
     }
   </script>
   <form action="?" id="form" method="POST" onsubmit="check()">
-    What is your username: <input type="text" name="name"><br><!-- replace later with session variable for username -->
+    
     What is your friend's username? <input type="text" name="friend"><br>
     <input type="submit" name="Add Friend">
   </form>
