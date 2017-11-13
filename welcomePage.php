d1c1aa74207e581b817e8e53c7d9254c75bb1a0d
sasbarkley <sasbarkley@gmail.com>
2017-11-13 10:34:28 -0500

    weirdness + welcomepage update

--- welcomePage.php
+++ welcomePage.php
@@ -31,13 +31,16 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">put icon here</a>
+      <a class="navbar-brand" href="#">
+        <span class="glyphicon glyphicon-ice-lolly-tasted" aria-hidden="true"></span>
+      </a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li><a href="./food_request.php">Request Food</a></li>
+        <li><a href="./contactus.php">Contact Us</a></li>
       </ul>
       <form class="navbar-form navbar-right" method="POST" action="profile.php">
         <div class="form-group">
@@ -46,18 +49,32 @@
         </div>
         <button type="submit" class="btn btn-default">Log In</button>
       </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="./signup.php">New User</a></li>
+      </ul>
     </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
   </nav>
   <div class="row text-center">
     <div class="col-md-2"></div>
+    <div class="col-md-8">
+      <h1>Welcome to King Hall Eats!</h1>
+      <h4>King Hall Eats is a service provided by midshipman, for midshipman as a way to organize the delivery of food from the one and only King Hall up to your room.</h4>
+    </div>
+    <div class="col-md-2">
+      Interested? Sign up <a href="./signup.php">today!</a>
+    </div>
+  </div>
+  <div class="row text-center">
     <div class="col-md-4">
-      What we're about
+      Selection you want
     </div>
     <div class="col-md-4">
-      Form to sign up
+      At the price you want
     </div>
-    <div class="col-md-2"></div>
+    <div class="col-md-4">
+      Without ever leaving your room
+    </div>
   </div>
 </body>
 </html>
