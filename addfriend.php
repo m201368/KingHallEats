f66629c03fda003971ef545c7a173b6bd1cc8062
gcdaves <33030032+gcdaves@users.noreply.github.com>
2017-11-20 09:55:43 -0500

    made session work

--- addfriend.php
+++ addfriend.php
@@ -1,6 +1,6 @@
 <?php
+  session_start();
   if(!isset($_SESSION['user'])) {
-    session_start();
     ?><script type="text/javascript">
       document.location = "welcomePage.php";
     </script><?php
