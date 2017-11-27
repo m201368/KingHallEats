8fd2beb4f4401a845e8bb96d6313d085bcd42691
gcdaves <33030032+gcdaves@users.noreply.github.com>
2017-11-27 10:46:10 -0500

    Tried to fix things

--- feed.php
+++ feed.php
@@ -1,11 +1,16 @@
 <?php
-
-function readUsers($fileName)
-{
+  session_start();
+  if(isset($_SESSION["user"])) {
+    $user = $_SESSION["user"];
+  } else {
+    header("location: welcomePage.php?fail=yes");
+  }
+  function readUsers($fileName)
+  {
     $array;
     $fp = fopen($fileName, 'r');   //open the file for reading
-  $line = fgets($fp);          // read lines
-  while (!feof($fp)) {
+    $line = fgets($fp);          // read lines
+    while (!feof($fp)) {
       $l = explode(",", $line);
       $array[$l[0]]['user'] = $l[0];
       $array[$l[0]]['pass'] = $l[1];
@@ -13,88 +18,67 @@ function readUsers($fileName)
       $array[$l[0]]['company'] = $l[3];
       $array[$l[0]]['room'] = $l[4];
       $line = fgets($fp);
-  }
+    }
     fclose($fp);                   //close the file
     return $array;
-}
-function checkInfo($array)
-{
-    if (isset($array[$_POST["user"]])) {
-        if ($array[$_POST["user"]]["pass"] == sha1($_POST["pass"])) {
-            return true;
-        }
-    } else {
-        return false;
-    }
-}
-$array  = readUsers("users.txt");
-$user = "hello";
-$pass = $_POST["pass"];
-// if (checkInfo($array)) {
-//     session_start();
-//     $_SESSION['user'] = $user;
-// } else {
-//     header("location: welcomePage.php?fail=yes");
-// }
+  }
+  $array  = readUsers("users.txt");
+  $user = $_SESSION["user"];
 
-function readRequests($fileName)
-{
+  function readRequests($fileName)
+  {
     $request;
     $fp = fopen($fileName, 'r');   //open the file for reading
-  $line = fgets($fp);          // read lines
-  $count = 0;
+    $line = fgets($fp);          // read lines
+    $count = 0;
     while (!feof($fp)) {
-        $l = explode(";", $line);
-        $request[$count]['user'] = $l[0];
-        $request[$count]['food'] = $l[1];
-        $request[$count]['comment'] = $l[2];
-        $request[$count]['time'] = $l[3];
-        $request[$count]['stat'] = $l[4];
-        $line = fgets($fp);
-        $count++;
+      $l = explode(";", $line);
+      $request[$count]['user'] = $l[0];
+      $request[$count]['food'] = $l[1];
+      $request[$count]['comment'] = $l[2];
+      $request[$count]['time'] = $l[3];
+      $request[$count]['stat'] = $l[4];
+      $line = fgets($fp);
+      $count++;
     }
     fclose($fp);
     //close the file
     return $request;
-}
-function agreetodeliver()
-{
-    header("location: deliver.php");
-}
+  }
+  function agreetodeliver()
+  {
+      header("location: deliver.php");
+  }
 function showyourfriends($name)
 {
-    $friend;
-    $fp = fopen("friends.txt", 'r');   //open the file for reading
+  $friend;
+  $fp = fopen("friends.txt", 'r');   //open the file for reading
   $line = fgets($fp);          // read lines
   $counter=0;
-    while (!feof($fp)) {
-        $l = explode(",", $line);
-        if ($l[0]==$name) {
-            $friend[$counter]=$l[1];
-            $counter++;
-        }
-        $line = fgets($fp);
+  while (!feof($fp)) {
+    $l = explode(",", $line);
+    if ($l[0]==$name) {
+      $friend[$counter]=$l[1];
+      $counter++;
     }
-    fclose($fp);                   //close the file
-    return $friend;
+    $line = fgets($fp);
+  }
+  fclose($fp);                   //close the file
+  return $friend;
 }
 
 
-// echo $user;
-$friends= showyourfriends($user);
-// echo"<pre>";
-// print_r($friends);
-// echo"</pre>";
+$friends = showyourfriends($user);
+
 ?>
 
 <html>
   <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title><?php echo $user; ?> Personalized Feed</title>
+    <title> Personalized Feed</title>
 
     <!-- Bootstrap -->
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -110,69 +94,64 @@ $friends= showyourfriends($user);
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="bootstrap/js/bootstrap.min.js"></script>
-  </head>
-  <body>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
   </head>
   <body>
-    <h1><b><u>Welcome to your Status Feed <?php echo $user; ?></u></b></h1>
+    <h1><b><u>Welcome to your Status Feed </u></b></h1>
 
-        <h3><b><u>Your Friend's Current Requests:</u></b></h3>
-        <div class="row">
-        <div class="col-md-offset-1 col-md-6">
-    <table class="table table-bordered">
-      <thead>
-        <tr><th>Friend's name</th><th>Request</th><th>Comments</th></tr>
-      </thead>
-      <tbody>
+    <h3><b><u>Your Friend's Current Requests:</u></b></h3>
+    <div class="row">
+      <div class="col-md-offset-1 col-md-6">
+        <table class="table table-bordered">
+          <thead>
+            <tr><th>Friend's name</th><th>Request</th><th>Comments</th></tr>
+          </thead>
+          <tbody>
+            <?php
+              $requests = readRequests("requests.txt");
+              echo"<pre>";
+              print_r($requests);
+              echo"</pre>";
+              for ($i=0; $i<sizeof($friends); $i++) {
+                  $name=$friends[$i];
 
-    <?php
-      $requests = readRequests("requests.txt");
-        for ($i=0; $i<sizeof($friends); $i++) {
-            $name=$friends[$i];
-
-            foreach ($requests as $key => $value) {
-                $requester=$requests[$key]["stat"];
-                $tester="incomplete";
-                if (strcmp($name, $requests[$key]["user"])==1 && strpos($requester, $tester)== true) {
-                    echo "<tr onclick=\"agreetodeliver()\"><td>".$requests[$key]["user"]."</td><td>".$requests[$key]["food"]."</td><td>".$requests[$key]["comment"]."</td></tr>";
-                }
-            }
-        }
-    ?>
-    </tbody>
-    </table>
-</div>
-</div>
+                  foreach ($requests as $key => $value) {
+                      $requester=$requests[$key]["stat"];
+                      $tester="incomplete";
+                      if ($name === $requests[$key]["user"]) {
+                          echo "<tr onclick=\"agreetodeliver()\"><td>".$requests[$key]["user"]."</td><td>".$requests[$key]["food"]."</td><td>".$requests[$key]["comment"]."</td></tr>";
+                      }
+                  }
+              }
+            ?>
+          </tbody>
+        </table>
+      </div>
+    </div>
     <h3><b><u>Your Current Requests:</u></b></h3>
     <div class="row">
-    <div class="col-md-offset-1 col-md-6">
-<table class="table table-bordered">
-      <thead>
-        <tr><th>Request</th><th>Comments</th><th>Status</th></tr>
-      </thead>
-      <tbody>
+      <div class="col-md-offset-1 col-md-6">
+        <table class="table table-bordered">
+          <thead>
+            <tr><th>Request</th><th>Comments</th><th>Status</th></tr>
+          </thead>
+          <tbody>
+            <?php
+              // $requests = readRequests("requests.txt");
+                foreach ($requests as $key ) {
+                  echo $requests[$key]["user"];
+                  echo $user;
+                    if ($requests[$key]["user"]==$user) {
+                        echo "<tr><td>".$requests[$key]["food"]."</td><td>".$requests[$key]["comment"]."</td><td>".$requests[$key]["stat"]."</td></tr>";
+                    }
+                }
 
-    <?php
-      $requests = readRequests("requests.txt");
+            ?>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </body>
+</html>
 
-        foreach ($requests as $key => $value) {
-            $requester=$requests[$key]["stat"];
-            $tester="incomplete";
-
-
-            if ($requests[$key]["user"]===$user && strpos($requester, $tester)== true) {
-                echo "<tr><td>".$requests[$key]["food"]."</td><td>".$requests[$key]["comment"]."</td><td>".$requests[$key]["stat"]."</td></tr>";
-            }
-        }
-
-    ?>
-    </tbody>
-    </table>
-</div>
-</div>
 
