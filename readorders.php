fc8525b31e4867a17a04bf35c455caf5a36002f5
Ben Birney <ben.birney@gmail.com>
2017-12-10 13:39:17 -0500

    Starting work on read_requests

--- readorders.php
+++ readorders.php
@@ -2,15 +2,21 @@
 <html>
 <!--create page and metas-->
 <head>
-  <style type="text/css">
-    table, td, th {
-      border: 1px solid black;
-    }
-  </style>
   <meta charset="UTF-8">
   <meta name="description" content="Table of All Requests for KingHallEats">
   <meta name="keywords" content="King, Hall, Eats, Food, Request, Table">
   <meta name="author" content="Lani Davis">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="http://grfx.cstv.com/graphics/school-logos/navy-lg.png">
+  <title>Food Request</title>
+  <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!--[if lt IE 9]>
+   <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+  <link type="text/css" rel="stylesheet" href="default.css">
   <title>All Food Request</title>
   </head>
 
@@ -42,7 +48,7 @@ $fp = fopen($filename, 'r');
 
   ?>
   <!--print table-->
-  <table>
+  <table class="table">
     <thead>
       <tr><th>Username</th><th>Food</th><th>Comments</th><th>Time</th><th>Status</th></tr>
     </thead>
