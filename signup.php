4c3534bdf02ee285365f12db5e6a7d25b1cf96bc
Ben Birney <ben.birney@gmail.com>
2017-12-10 15:28:37 -0500

    Finished up signup.php

--- signup.php
+++ signup.php
@@ -1,3 +1,6 @@
+<!DOCTYPE html>
+<html>
+
 <?php
   function readUsers($fileName){
     $array;
@@ -38,7 +41,31 @@
 
 ?>
 
-<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="description" content="Sign Up for KHE">
+  <meta name="keywords" content="signup, king, hall, kinghalleats">
+  <meta name="author" content="Chris Daves">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <title>Sign Up</title>
+
+  <!-- Bootstrap -->
+  <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+  <link type="text/css" rel="stylesheet" href="default.css">
   <script type="text/javascript">
     //make sure all boxes have been set
     function check(){
@@ -57,13 +84,56 @@
       }
     }
   </script>
-  <form action="?" id="form" method="POST" onsubmit="check()">
-    Name: <input type="text" name="names"><br>
-    Company: <input type="text" name="company"><br>
-    Room Number: <input type="text" name="room"><br>
-    Username: <input type="text" name="user"><br>
-    Password: <input type="text" name="pass"><br>
-    <input type="submit" name="Create Profile">
-  </form>
-</html>
+</head>
+<body>
+  <nav class="navbar navbar-custom navbar-fixed-top">
+    <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
 
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <a class="navbar-brand" href="./welcomePage.php">
+        <span class="glyphicon glyphicon-ice-lolly-tasted" aria-hidden="true"></span>
+      </a>
+      <ul class="nav navbar-nav">
+        <li><a href="./contactus.php">Contact Us</a></li>
+      </ul>
+      <form class="navbar-form navbar-right" method="POST" action="profile.php">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Username" name="user">
+          <input type="password" class="form-control" placeholder="Password" name="pass">
+        </div>
+        <button type="submit" class="btn btn-default">Log In</button>
+      </form>
+    </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+  </nav>
+  <div class="row">
+    <div class="col-md-4"></div>
+    <div class="col-md-4 text-center">
+      <br><br>
+      <div class="jumbotron text-center">
+      <h3>Sign Up!</h3>
+      <br>
+      <form action="?" id="form" method="POST" onsubmit="check()">
+        Name: <input type="text" class="form-control" style="max-width:50%;margin-left:auto;margin-right:auto;" name="names"><br>
+        Company: <input type="text" class="form-control" style="max-width:50%;margin-left:auto;margin-right:auto;" name="company"><br>
+        Room Number: <input type="text" class="form-control" style="max-width:50%;margin-left:auto;margin-right:auto;" name="room"><br>
+        Username: <input type="text" class="form-control" style="max-width:50%;margin-left:auto;margin-right:auto;" name="user"><br>
+        Password: <input type="text" class="form-control" style="max-width:50%;margin-left:auto;margin-right:auto;" name="pass"><br>
+        <button type="submit" class="btn btn-default">Create Profile</button>
+      </form>
+    </div>
+    </div>
+    <div class="col-md-4"></div>
+  </div>
+</body>
+</html>
