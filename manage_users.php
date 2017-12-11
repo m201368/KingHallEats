1bd5427ea8311edf0d09d0e3b6ef9b466948ed7b
Ben Birney <ben.birney@gmail.com>
2017-12-11 00:38:16 -0500

    Added bootstrap / admin navbar

--- manage_users.php
+++ manage_users.php
@@ -15,7 +15,6 @@
       exit();
     }
   }
-
 ?>
 
 <html>
@@ -37,8 +36,6 @@
   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <!-- Include all compiled plugins (below), or include individual files as needed -->
-  <script src="bootstrap/js/bootstrap.min.js"></script>
-  <script type="text/javascript" src="pscripts.js"></script>
   <link href="bootstrap/css.bootstrap.min.css" rel="stylesheet">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery.min.js"></script>
   <script src="bootstrap/js/bootstrap.min.js"></script>
@@ -47,6 +44,50 @@
 </head>
 
 <body>
+  <nav class="navbar navbar-custom">
+    <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <a class="navbar-brand" href="./welcomePage.php">
+        <span class="glyphicon glyphicon-ice-lolly-tasted" aria-hidden="true"></span>
+      </a>
+      <ul class="nav navbar-nav">
+        <li><a href="./contactus.php">Contact Us</a></li>
+      </ul>
+      <form class="navbar-form navbar-right" role="search">
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="Search" name="q">
+            <div class="input-group-btn">
+              <button class="btn btn-default form-control" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+            </div>
+        </div>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="">Admin
+          <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="./manage_users.php">Manage Users</a></li>
+            <li><a href="./stats.php">Statistics</a></li>
+          </ul>
+        </li>
+        <li><a href="./food_request.php">Request Food</a></li>
+        <li><a href="./feed.php">NewsFeed</a></li>
+        <li><a href="./updateprof.php">Profile</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+  </nav>
   <?php
   //creates user given username, password, full name, and access level from form
   if (isset($_SESSION['user'])) {
@@ -81,11 +122,11 @@
         <div class="col-md-6 text-center">
           <br>
           Add a user:
-          <input type="text" name="addUser" placeholder="Username">
-          <input type="password" name="pass" placeholder="Password">
-          <input type="text" name="name" placeholder="Full Name">
-          <input type="text" name="name" placeholder="Room">
-          <select name="access">
+          <input type="text" name="addUser" class="form-control" placeholder="Username" style="max-width:50%;margin-left:auto;margin-right:auto;">
+          <br><input type="password" name="pass" class="form-control" placeholder="Password" style="max-width:50%;margin-left:auto;margin-right:auto;">
+          <br><input type="text" name="name" class="form-control" placeholder="Full Name" style="max-width:50%;margin-left:auto;margin-right:auto;">
+          <br><input type="text" name="name" class="form-control" placeholder="Room" style="max-width:50%;margin-left:auto;margin-right:auto;">
+          <br><select name="access">
             <option value="">Access Level</option>
             <option value="admin">Admin</option>
             <option value="user">User</option>
@@ -94,7 +135,7 @@
         <div class="col-md-6 text-center">
           <br>
           Remove a User:
-          <input type="text" name="removeUser" placeholder="Username">
+          <br><input type="text" class="form-control" name="removeUser" placeholder="Username" style="max-width:50%;margin-left:auto;margin-right:auto;">
         </div>
       </div>
       <div class="row">
