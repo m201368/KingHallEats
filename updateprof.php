04bec66c02acf8d4563024b17c9411a091408e63
Ben Birney <ben.birney@gmail.com>
2017-12-10 21:11:19 -0500

    Added bootstrap/navbar

--- updateprof.php
+++ updateprof.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html>
 <!--
  +Description: PHP file that allows users to contact the staff for feedback.
@@ -30,48 +31,58 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="./welcomePage.php">
-        <span class="glyphicon glyphicon-ice-lolly-tasted" aria-hidden="true"></span>
-      </a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <a class="navbar-brand" href="./welcomePage.php">
+        <span class="glyphicon glyphicon-ice-lolly-tasted" aria-hidden="true"></span>
+      </a>
       <ul class="nav navbar-nav">
-        <li><a href="./food_request.php">Request Food</a></li>
         <li><a href="./contactus.php">Contact Us</a></li>
       </ul>
-      <form class="navbar-form navbar-right" method="POST" action="profile.php">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Username" name="user">
-          <input type="password" class="form-control" placeholder="Password" name="pass">
+      <form class="navbar-form navbar-right" role="search">
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="Search" name="q">
+            <div class="input-group-btn">
+              <button class="btn btn-default form-control" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+            </div>
         </div>
-        <button type="submit" class="btn btn-default">Log In</button>
       </form>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="./signup.php">New User</a></li>
+        <li><a href="./food_request.php">Request Food</a></li>
+        <li><a href="./feed.php">NewsFeed</a></li>
+        <li><a href="./updateprof.php">Profile</a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
   </nav>
-  <div class="container-fluid text-center">
-    <h2>Update Your Profile</h2>
-    <p></p>
-  </div>
-  <div class="container">
-    <div class="row text-center">
-      <div class="col-md-8 col-md-offset-2">
-        <form action="#">
-          <div class="form-group">
-            <input type="text" class="form-control" placeholder="Full Name">
-            <input type="text" class="form-control" placeholder="Company">
-            <input type="text" class="form-control" placeholder="Room Number">
-            <input type="username" class="form-control" placeholder="Username">
-            <input type="password" class="form-control" placeholder="Password">
-            <input type="submit" class="form-control" value="Submit Changes">
-          </div>
-        </form>
+  <br>
+  <div class="row">
+    <div class="col-md-1"></div>
+    <div class="col-md-2 jumbotron">
+      <h3 class="text-center"><?php echo $_SESSION['username'];?></h3><br>
+      <div style="max-width:75%;margin-left:auto;margin-right:auto;background-color:white;">
+        <img src="IDONTKNOW" alt="Profile Picture">
       </div>
+      <br>
+      Name: <?php echo $data[$_SESSION['username']]['fullname'];?><br>
+      Company: <?php echo $data[$_SESSION['username']]['company'];?><br>
+      Room: <?php echo $data[$_SESSION['username']]['room'];?><br>
+      Bio: <?php echo $data[$_SESSION['username']]['bio'];?><br>
+    </div>
+    <div class="col-md-9 text-center">
+      <h3>Update Profile:</h3><br>
+      <form action="#">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Full Name"  style="max-width:40%;margin-left:auto;margin-right:auto;">
+          <br><input type="text" class="form-control" placeholder="Company"  style="max-width:40%;margin-left:auto;margin-right:auto;">
+          <br><input type="text" class="form-control" placeholder="Room Number"  style="max-width:40%;margin-left:auto;margin-right:auto;">
+          <br><input type="username" class="form-control" placeholder="Username"  style="max-width:40%;margin-left:auto;margin-right:auto;">
+          <br><input type="password" class="form-control" placeholder="Password"  style="max-width:40%;margin-left:auto;margin-right:auto;">
+          <br><button type="submit" class="btn btn-default">Update Profile</button>
+        </div>
+      </form>
     </div>
   </div>
 </body>
