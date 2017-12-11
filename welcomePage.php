f37cd5d5c2877dba6f0dfa462410356edd01e665
Ben Birney <ben.birney@gmail.com>
2017-12-11 14:19:41 -0500

    Updated description / form actions

--- welcomePage.php
+++ welcomePage.php
@@ -76,8 +76,6 @@
           Enjoy!
         </p>
         <br>
-        <br>
-        <br>
       </div>
     </div>
     <div class="col-md-4">
@@ -89,7 +87,7 @@
           <input type="text" class="form-control" name="room" style="max-width:50%;margin-left:auto;margin-right:auto;" placeholder="Room #"><br>
           <input type="text" class="form-control" name="user" style="max-width:50%;margin-left:auto;margin-right:auto;" placeholder="Username"><br>
           <input type="password" class="form-control" name="pass" style="max-width:50%;margin-left:auto;margin-right:auto;" placeholder="Password"><br>
-          <input type="password" class="form-control" name="allergy" style="max-width:50%;margin-left:auto;margin-right:auto;" placeholder="Allergies"><br>
+          <input type="text" class="form-control" name="allergy" style="max-width:50%;margin-left:auto;margin-right:auto;" placeholder="Allergies"><br>
          <button type="submit" class="btn btn-default">Sign Up</button>
         </form>
        <br>
