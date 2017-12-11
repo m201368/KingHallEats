3d327afe47d1dd73e9fde8e530ff4caccd5300bf
m201368 <33030039+m201368@users.noreply.github.com>
2017-12-11 14:11:32 -0500

    added content to what we're about

--- welcomePage.php
+++ welcomePage.php
@@ -56,9 +56,27 @@
     <div class="col-md-2"></div>
     <div class="col-md-4">
       <div class="jumbotron">
-        <h2 class="text-center">What we're about</h4><br>
-        King Hall Eats is a service provided by midshipman, for midshipman as a way to organize the delivery of food from the one and only King Hall up to your room.
-        <br><br>***We should probably make this longer***<br><br><br><br><br><br><br><br><br><br><br><br><br>
+          <h2 class="text-center">What we're about</h2><br>
+        <p style="font-size:12pt">
+          King Hall Eats is a service provided by midshipman, for midshipman as a way to organize the delivery of food from the one and only King Hall up to your room.
+        </p>
+        <p style="font-size:12pt">
+          Designed by midshipmen, we understand the struggle of walking down to King Hall to pick up food. This service is a quick way of getting other people to pick up your food on the way.
+        </p>
+        <p style="font-size:12pt">
+          Simply submit a request and put how much you are willing to pay for the delivery in the comments.
+        </p>
+        <p style="font-size:12pt">
+          Wait for another midshipman looking to make some cash already on the way to King Hall to agree to deliver your food.
+        </p>
+        <p style="font-size:12pt">
+          Wait for your delivery.
+        </p>
+        <p style="font-size:12pt">
+          Enjoy!
+        </p>
+        <br>
+        <br>
       </div>
     </div>
     <div class="col-md-4">
