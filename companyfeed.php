03f0043d0dc9d3aa0d0f1f35326f948bed8048af
Ben Birney <ben.birney@gmail.com>
2017-12-11 10:55:23 -0500

    Added navbar / bootstrap

--- companyfeed.php
+++ companyfeed.php
@@ -69,57 +69,107 @@ $array  = readUsers("users.txt");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <link type="text/css" rel="stylesheet" href="default.css">
 
   </head>
   <body>
-    <h1><b><u>Company Feed</u></b></h1>
+    <nav class="navbar navbar-custom">
+      <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <a class="navbar-brand" href="./welcomePage.php">
+          <span class="glyphicon glyphicon-ice-lolly-tasted" aria-hidden="true"></span>
+        </a>
+        <ul class="nav navbar-nav">
+          <li><a href="./contactus.php">Contact Us</a></li>
+        </ul>
+        <form class="navbar-form navbar-right" role="search">
+          <div class="input-group">
+              <input type="text" class="form-control" placeholder="Search" name="q">
+              <div class="input-group-btn">
+                <button class="btn btn-default form-control" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+              </div>
+          </div>
+        </form>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="./food_request.php">Request Food</a></li>
+          <li><a href="./feed.php">NewsFeed</a></li>
+          <li><a href="./updateprof.php">Profile</a></li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+    <br>
+
+    <div class="row">
+      <div class="col-md-1"></div>
+      <div class="col-md-2 jumbotron">
+        <h3 class="text-center"><?php echo $_SESSION['username'];?></h3><br>
+        <div style="max-width:75%;margin-left:auto;margin-right:auto;background-color:white;">
+          <img src="IDONTKNOW" alt="Profile Picture">
+        </div>
+        <br>
+        Name: <?php echo $data[$_SESSION['username']]['fullname'];?><br>
+        Company: <?php echo $data[$_SESSION['username']]['company'];?><br>
+        Room: <?php echo $data[$_SESSION['username']]['room'];?><br>
+        Bio: <?php echo $data[$_SESSION['username']]['bio'];?><br>
+      </div>
+      <div class="col-md-9">
+    <h2 class="text-center">Status Feed</h2>
 
     <form class="form"method='post' action="" >
-    <label for='search'>Search for a request by Company</label>
-    <select name="selectex">
-<option value="1">1</option>
-<option value="2">2</option>
-<option value="3">3</option>
-<option value="4">4</option>
-<option value="5">5</option>
-<option value="6">6</option>
-<option value="7">7</option>
-<option value="8">8</option>
-<option value="9">9</option>
-<option value="10">10</option>
-<option value="11">11</option>
-<option value="12">12</option>
-<option value="13">13</option>
-<option value="14">14</option>
-<option value="15">15</option>
-<option value="16">16</option>
-<option value="17">17</option>
-<option value="18">18</option>
-<option value="19">19</option>
-<option value="20">20</option>
-<option value="21">21</option>
-<option value="22">22</option>
-<option value="23">23</option>
-<option value="24">24</option>
-<option value="25">25</option>
-<option value="26">26</option>
-<option value="27">27</option>
-<option value="28">28</option>
-<option value="29">29</option>
-<option value="30">30</option>
-</select>
-       <input type="submit" value="Search">
+    <label for='search'>Customize by Company</label>
+    <select name="selectex" class="form-control" style="max-width:10%">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+      <option value="6">6</option>
+      <option value="7">7</option>
+      <option value="8">8</option>
+      <option value="9">9</option>
+      <option value="10">10</option>
+      <option value="11">11</option>
+      <option value="12">12</option>
+      <option value="13">13</option>
+      <option value="14">14</option>
+      <option value="15">15</option>
+      <option value="16">16</option>
+      <option value="17">17</option>
+      <option value="18">18</option>
+      <option value="19">19</option>
+      <option value="20">20</option>
+      <option value="21">21</option>
+      <option value="22">22</option>
+      <option value="23">23</option>
+      <option value="24">24</option>
+      <option value="25">25</option>
+      <option value="26">26</option>
+      <option value="27">27</option>
+      <option value="28">28</option>
+      <option value="29">29</option>
+      <option value="30">30</option>
+      </select>
+      <button type="submit" class="btn btn-default">Submit</button>
 
     </form>
 
-    <h3><b><u>Current Requests:</u></b></h3>
-    <div class="row">
-    <div class="col-md-offset-1 col-md-6">
-<table class="table table-bordered">
-      <thead>
-        <tr><th>Person Requesting</th><th>Food Requested</th><th>Comments</th><th>Time</th></tr>
-      </thead>
-      <tbody>
+        <table class="table table-bordered" style="max-width:75%;margin-left:auto;margin-right:auto;">
+        <thead>
+          <tr><th>Person Requesting</th><th>Food Requested</th><th>Comments</th><th>Time</th></tr>
+        </thead>
+        <tbody>
 
     <?php
     $requests = readRequests("requests.txt");
@@ -152,5 +202,7 @@ foreach ($array as $key => $value) {
     ?>
     </tbody>
     </table>
+  </div>
 </div>
-</div>
+</body>
+</html>
