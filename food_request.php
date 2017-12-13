8be7caff14113973c403dc9c5e04dd14c230010e
m201368 <33030039+m201368@users.noreply.github.com>
2017-12-12 20:46:16 -0500

    fixed sidebar

--- food_request.php
+++ food_request.php
@@ -102,11 +102,12 @@ Last Edited: 28 OCT 2017
   <div class="row">
     <div class="col-md-1"></div>
     <div class="col-md-2 jumbotron">
-      Name: <?php echo $users[$_COOKIE['user']]['name'];?><br>
-      Company: <?php echo $users[$_COOKIE['user']]['company'];?><br>
-      Room: <?php echo $users[$_COOKIE['user']]['room'];?><br>
-      Allergies: <?php echo $users[$_COOKIE['user']]['allergy'];?><br>
-      Favorite Food: <?php echo $users[$_COOKIE['user']]['favfood'];?><br>
+       <h3 class="text-center"><?php echo $_COOKIE['user'];?></h3><br>
+      Name: <?php echo $array[$user]["name"];?><br>
+      Company: <?php echo $array[$user]["company"];?><br>
+      Room: <?php echo $array[$user]["room"];?><br>
+      Allergies: <?php echo $array[$user]["allergy"];?><br>
+      Favorite Food: <?php echo $array[$user]["favfood"];?><br>
     </div>
   <?php
   if (isset($_POST['sFood'])) {
