aeafd35dd5d76bc5ebc6242e04390c0145469f4e
gcdaves <33030032+gcdaves@users.noreply.github.com>
2017-12-12 21:12:23 -0500

    Update profile.php

--- profile.php
+++ profile.php
@@ -63,8 +63,7 @@ function showyourfriends($name){
 }
 
 $array  = readUsers("users.txt");
-$user = $_POST['user'];
-$pass = $_POST['pass'];
+$user = $_COOKIE["user"];
 
 if(isset($_POST['pass']) && (!isset($array[$_COOKIE['user']]) || sha1($_POST['pass']) != $array[$_POST['user']]['pass'])){
   require_once('logout.php');
