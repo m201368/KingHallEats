4bd5005568da06ad5d31864acd5b9898aadd305d
Ben Birney <ben.birney@gmail.com>
2017-12-15 11:27:12 -0500

    brbr

--- updateprof.php
+++ updateprof.php
@@ -115,7 +115,7 @@
     </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
   </nav>
-  <br>
+  <br><br><br>
   <div class="row">
     <div class="col-md-1"></div>
     <div class="col-md-2 jumbotron">
