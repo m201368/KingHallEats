3868cf2a9417968b0f8c707fff8a8421efb4fddb
Ben Birney <ben.birney@gmail.com>
2017-12-15 11:12:10 -0500

    Static Navbar

--- search.php
+++ search.php
@@ -89,11 +89,11 @@ function readRequests($fileName){
     }
   </script>
   <body>
-    <nav class="navbar navbar-custom">
-<div class="container-fluid">
-<!-- Brand and toggle get grouped for better mobile display -->
-<div class="navbar-header">
-  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+    <nav class="navbar navbar-custom navbar-fixed-top">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
     <span class="sr-only">Toggle navigation</span>
     <span class="icon-bar"></span>
     <span class="icon-bar"></span>
